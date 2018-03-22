efabfc4788a4df2bdbc8deae9fcd995503401dfd
Greg3883 <gregoire.bejjaji@gmail.com>
2018-03-22 16:09:28 +0100

    Exo 4 Terminé :+1:

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
@@ -9,8 +9,6 @@ import com.acme.mailreader.utils.DateIncorrecteException;
 
 public class MailTest {
 	
-
-	@Ignore
 	@Test(expected=DateIncorrecteException.class)
 	public final void erreurSiDateAvant1979() throws DateIncorrecteException {
 		Mail mail = new Mail.Builder("test").date(Instant.parse("1900-04-05T13:33:12Z")).build();
